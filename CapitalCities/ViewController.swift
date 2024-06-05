d339138edd30bfef42778161ea185a3520f6c7ce
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-06-05 17:05:42 -0300

    Initial Commit

---
+++ CapitalCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Rodrigo Cavalcanti on 05/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
