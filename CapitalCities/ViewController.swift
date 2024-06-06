887e81af01a85dd9a0dad5f1e84e9952fedd822c
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-06-06 11:39:05 -0300

    Customized annotations and added function to react to accessory taps

--- CapitalCities/ViewController.swift
+++ CapitalCities/ViewController.swift
@@ -7,7 +7,7 @@
 
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet var mapView: MKMapView!
     
     override func viewDidLoad() {
@@ -44,7 +44,45 @@ class ViewController: UIViewController {
         mapView.addAnnotations([rioDeJaneiro, saoPaulo, brasilia, salvador, fortaleza, beloHorizonte, manaus, curitiba, recife, portoAlegre, goiania, belem, cuiaba, natal, teresina, joaoPessoa, maceio, aracaju, palmas, macapa, rioBranco, portoVelho, saoLuis, boaVista, floripa, campoGrande, vitoria])
 
     }
-
+    func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
+        // If the annotation isn't from a capital city, it must return nil so iOS uses a default view.
+        guard annotation is Capital else { return nil }
+        
+        // Define a reuse identifier. This is a string that will be used to ensure we reuse annotation views as much as possible.
+        let identifier = "Capital"
+        
+        // Try to dequeue an annotation view from the map view's pool of unused view
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            // If it isn't able to find a reusable view, create a new one using MKPinAnnotationView and sets its canShowCallout property to true. This triggers the popup with the city name.
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            
+            // Create a new UIButton using the built-in .detailDisclosure type. This is a small blue "i" symbol with a circle around it.
+            
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView?.rightCalloutAccessoryView = btn
+            
+        } else {
+            // If it can reuse a view, update that view to use a different annotation.
+            annotationView?.annotation = annotation
+        }
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        // what map view sent it (There's only one in this project), what annotation view the button came from (this is useful), what button was tapped.
+        
+        guard let capital = view.annotation as? Capital else { return }
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
 
 }
 
